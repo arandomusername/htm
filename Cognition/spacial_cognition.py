6299cf37831c71054c8881ab25ba574dc21ec26f
Martin <vi.fri.martin@gmail.com>
2014-06-14 17:17:21 +0200

    debugging

--- Cognition/spacial_cognition.py
+++ Cognition/spacial_cognition.py
@@ -50,14 +50,13 @@ class SpacialCognitor():
     def booster(self):
         # at least 5% of all columns must be active and then adds
         quantity_needed = (len(self.region.columns)*5)/100
+        smallest_overlap = None
+        boosted_columns = []
 
         while len(self.winner) < quantity_needed:
-            smallest_overlap = None
-            boosted_columns  = []
-
             if smallest_overlap is None or len(boosted_columns) == 0:
-                smallest_overlap = self.get_smallest_overlap() -1
-                boosted_columns  = self.region.get_columns_by_overlap(smallest_overlap)
+                smallest_overlap = self.get_smallest_overlap() - 1
+                boosted_columns = self.region.get_columns_by_overlap(smallest_overlap)
 
             boosted_columns.sort(key=lambda x: x.last_activation, reverse=True)
             self.winner.append(boosted_columns.pop(0))
@@ -65,15 +64,20 @@ class SpacialCognitor():
     def update_activation(self):
         for column in self.region.columns:
             if column in self.winner:
-                column.increase_last_activation()
-            else:
                 column.reset_last_activation()
+            else:
+                column.increase_last_activation()
 
     def get_smallest_overlap(self):
         overlap = None
+
         for column in self.winner:
             if column.dendrit_segment.overlap < overlap or overlap is None:
                 overlap = column.dendrit_segment.overlap
+
+        if overlap is None:
+            overlap = 1
+
         return overlap
 
     def spacial_learning(self):
