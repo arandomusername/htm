e0fc88c082038ca3263a57faaae43df97b256fb5
arandomusername <vi.fri.martin@gmail.com>
2015-04-07 17:46:49 +0200

    selection with inhibition

--- region.py
+++ region.py
@@ -13,15 +13,20 @@ class region(object):
         self.columns = [[column([x, y]) for x in range(self.size)]
                                         for y in range(self.size)]
 
-    def get_activation_matrix(self):
+    def get_activation_scores(self):
         act_arr = np.zeros(shape=(self.size, self.size))
         for x in range(self.size):
             for y in range(self.size):
                 act_arr[x, y] = self.columns[x][y].get_activity_score()
         return act_arr
 
-    def update_active_column_matrix(self):
-        pass
+    def update_activation(self, active_list):
+        self.active_columns.fill(0)
+        for coor in active_list:
+            self.active_columns[coor[0], coor[1]] = 1
+
+    def get_activation(self):
+        return self.active_columns
 
     def update_active_neuron_matrix(self):
         pass
--- temporal_pooler.py
+++ temporal_pooler.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class temporal_pooler:
-    activation_percentage = .3
+    activ_percent = .3
 
     def __init__(self, reg):
         self.region = reg
@@ -12,11 +12,28 @@ class temporal_pooler:
 
     def run(self, region):
         self.__set_region(region)
-        self.select_activated()
+        region.update_activation(self.select_activated())
 
     def select_activated(self):
-        activation_array = self.region.get_activation_matrix()
-        pass
+        activation = self.region.get_activation_matrix()
+        inhibition_x = int(activation.shape()[0] * self.activ_percent)
+        inhibition_y = int(activation.shape()[1] * self.activ_percent)
+        max_list     = []
+
+        for x in range(activation.shape()[0] - inhibition_x):
+            x_max = x + inhibition_x
+            for y in range(activation.shape()[1] - inhibition_y):
+
+                y_max = y + inhibition_y
+                sub_ar = activation[x:x_max, y:y_max]
+                coor_max = np.unravel_index(sub_ar.argmax(), sub_ar.shape)
+
+                coor_max[0] += x
+                coor_max[1] += y
+
+                max_list.append(coor_max)
+
+        return max_list
 
     def learn(self):
         pass
